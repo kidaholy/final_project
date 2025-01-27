254d5e1b2efce5628ce9ebf5e2c90c0da9c67a72
kidaholy <kidayos2014@gmail.com>
2025-01-27 23:28:34 +0300

    making secure authentication for admin

--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,10 +1,20 @@
+import { useNavigate } from 'react-router-dom'
 import {useAuth} from '../context/authContext.jsx'
 
 function AdminDashboard() {
   const {user} = useAuth()
+  const navigate = useNavigate()
+  const {loading} = useAuth()
+  if(loading) {
+    return <div>Loading...</div>
+  }
+  if(!user) {
+    navigate("/login")
+  }
   return (
     <div className='text-5xl text-teal-400'>
-      AdminDashoard {user.name}
+      {/*  {user && user.name} */}
+      AdminDashoard
     </div>
   )
 }
