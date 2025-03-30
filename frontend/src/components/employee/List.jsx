d34c8a1681789aa621fd06749fd177c1911b7cd3
kidaholy <kidayos2014@gmail.com>
2025-03-30 22:34:55 +0300

    adding filter or search method for employee

--- frontend/src/components/employee/List.jsx
+++ frontend/src/components/employee/List.jsx
@@ -7,6 +7,7 @@ import DataTable from "react-data-table-component";
 const List = () => {
   const [employees, setEmployees] = useState([]);
   const [empLoading, setEmpLoading] = useState(false);
+  const [filteredEmployee, setFilteredEmployee] = useState([]);
 
   useEffect(() => {
     const fetchEmployees = async () => {
@@ -28,11 +29,23 @@ const List = () => {
             sno: sno++,
             dep_name: emp.department?.dep_name || <strong>NULL</strong>,
             name: emp.userId?.name || <strong>NULL</strong>,
-            dob: emp.dob ? new Date(emp.dob).toLocaleDateString() : <strong>NULL</strong>,
-            profileImage: <img src={`http://localhost:5000/${emp.userId?.profileImage}`} width={40} height={40} className="rounded-full" /> || <strong>NULL</strong>,
+            dob: emp.dob ? (
+              new Date(emp.dob).toLocaleDateString()
+            ) : (
+              <strong>NULL</strong>
+            ),
+            profileImage: (
+              <img
+                src={`http://localhost:5000/${emp.userId?.profileImage}`}
+                width={40}
+                height={40}
+                className="rounded-full"
+              />
+            ) || <strong>NULL</strong>,
             action: <EmployeeButtons _id={emp._id} />,
           }));
           setEmployees(data);
+          setFilteredEmployee(data);
         }
       } catch (error) {
         console.error("Error fetching employees:", error);
@@ -44,12 +57,25 @@ const List = () => {
     fetchEmployees();
   }, []);
 
+  const handleFilter = (e) => {
+    const records = employees.filter((emp) => (
+      emp.name.toLowerCase().includes(e.target.value.toLowerCase())
+    ))
+    setFilteredEmployee(records);
+  };
+
   return (
     <div className="p-5">
       <div className="text-center mb-6">
         <h3 className="text-2xl font-bold">Manage Employees</h3>
       </div>
-      <div className="flex justify-between items-center mb-4">
+      <div className="flex justify-between items-center">
+        <input
+          type="text"
+          placeholder="Search By Emp Name"
+          className="px-4 py-0.5 border"
+          onChange={handleFilter}
+        />
         <Link
           to="/admin-dashboard/add-employee"
           className="px-4 py-2 bg-teal-600 rounded-lg text-white hover:bg-teal-700 transition-colors"
@@ -60,7 +86,7 @@ const List = () => {
       <div className="bg-white rounded-lg shadow">
         <DataTable
           title="Employee List"
-          data={employees}
+          data={filteredEmployee}
           columns={columns}
           pagination
           progressPending={empLoading}
